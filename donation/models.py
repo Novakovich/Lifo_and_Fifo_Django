c7478b252498260c115db69c0989c77455b839f6
Maksim <teramor1986@gmail.com>
2022-07-28 17:19:39 +0300

    Django Lifo state

--- donation/models.py
+++ donation/models.py
@@ -8,10 +8,10 @@ class Office(models.Model):
 class Donate(models.Model):
     name = models.CharField(max_length=100)
     amount = models.IntegerField()
-    state = models.CharField(max_length=100,
-        choices=(('Available', 'Available'),
-                 ('Requested', 'Requested'),
-                 ('Booked', 'Booked'),
-                 ('Shipped', 'Shipped')), default='Available'
+    state = models.CharField(max_length=100, choices=(
+             ('Available', 'Available'),
+             ('Requested', 'Requested'),
+             ('Booked', 'Booked'),
+             ('Shipped', 'Shipped')), default='Available'
     )
     office = models.ForeignKey(Office, on_delete=models.CASCADE, null=True)
--- donation/views.py
+++ donation/views.py
@@ -11,21 +11,18 @@ def donate(request):
         amount=request.POST["amount"],
         office=Office.objects.order_by('?')[0]
     )
-
     return render(request, 'donate.html')
 
 def donation(request):
-    donate = Donate.objects.order_by('id').first()
+    donate = Donate.objects.order_by('id').filter(state='Available').first()
     if not donate:
         return render(request, 'no_data.html')
-    donate.delete()
+    donate.state = 'Booked'
+    donate.save()
     return render(request, 'donation.html', {"donate": donate})
 
 def list(request):
-
-    context = {
-        'data': []
-    }
+    context = {'data': []}
     donate = Donate.objects.all()
     context['data'] = donate
     return render(request, 'list.html', context)
