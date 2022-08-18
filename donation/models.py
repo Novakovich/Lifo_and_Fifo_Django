8085412d0cb0edbf3d9415aaa489714c6aae12fd
Maksim <teramor1986@gmail.com>
2022-08-18 11:26:09 +0300

    Django CharityBar changed some html css and add multipy donation

--- donation/models.py
+++ donation/models.py
@@ -29,14 +29,17 @@ class Request(models.Model):
     request_amount = models.IntegerField(default=0)
 
 
-class Request_item(models.Model):
+class Item(models.Model):
     name_item = models.CharField(max_length=100, default=0)
+    amount_item = models.IntegerField(default=0)
     request_hash = models.ForeignKey(Request, on_delete=models.CASCADE, null=True)
     office = models.ForeignKey(Office, on_delete=models.CASCADE, null=True)
+
 
 def full_storage(sender, instance, **kwargs):
     state = sender.objects.filter(state='Available', office=instance.office_id).aggregate(Sum('amount'))
     instance.office.office_count = state['amount__sum']
     instance.office.save()
-models.signals.post_save.connect(receiver=full_storage, sender=Donate)
 
+
+models.signals.post_save.connect(receiver=full_storage, sender=Donate)
--- donation/views.py
+++ donation/views.py
@@ -1,10 +1,11 @@
 from django.db import transaction
 from django.shortcuts import render
-from donation.models import Donate, Office, Request, Request_item
+from donation.models import Donate, Office, Request, Item
+
 
 def home_page(request):
     state = Office.objects.order_by('office_count').last()
-    if state.office_count == None:
+    if state.office_count is None:
         office = 0
     else:
         office = state.office_count
@@ -14,10 +15,12 @@ def home_page(request):
     }
     return render(request, 'main.html', context)
 
+
 def session_office(request):
     office_id = request.session["office"] = request.POST["office"]
     place = Office.objects.get(id=office_id)
     return render(request, 'main.html', {"place": place})
+
 
 def request(request):
     Request.objects.create(request_amount=request.POST["request"])
@@ -26,6 +29,7 @@ def request(request):
         "request": range(n.request_amount),
     }
     return render(request, 'number.html', context)
+
 
 @transaction.atomic
 def donate(request):
@@ -36,6 +40,7 @@ def donate(request):
     )
     return render(request, 'donate.html')
 
+
 @transaction.atomic
 def donation(request):
     donate = Donate.objects.select_for_update().order_by('id').filter(state='Available').first()
@@ -45,19 +50,27 @@ def donation(request):
     donate.save()
     return render(request, 'donation.html', {"donate": donate})
 
+
 def list(request):
-    context = {'data': []}
+    context = {
+        'data': [],
+        'data_request': []
+               }
     donate = Donate.objects.all()
+    req = Item.objects.all()
     context['data'] = donate
+    context['data_request'] = req
     return render(request, 'list.html', context)
 
+
+@transaction.atomic
 def correct_request(request):
     req = Request.objects.order_by('id').last()
-    number = Request.objects.order_by('request_amount').last()
-    number_req = range(number.request_amount)
+    number_req = range(req.request_amount)
     for i in number_req:
-        Request_item.objects.create(
+        Item.objects.create(
             name_item=request.POST[f'name{i}'],
+            amount_item=request.POST[f'amount{i}'],
             office_id=request.session["office"],
             request_hash_id=req.id
         )
