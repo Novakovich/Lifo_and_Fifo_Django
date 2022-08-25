53e11ee33e95d8b2c42647c9e7a3615b633d088c
Maksim <teramor1986@gmail.com>
2022-08-25 14:25:55 +0300

    Django CharityBar add Admin model + fields + readonly_fields

--- donation/admin.py
+++ donation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from donation.models import Description
 
-# Register your models here.
+
+class DescriptionAdmin(admin.ModelAdmin):
+
+    fields = ['name_item', 'amount_item', 'request_hash_id']
+    list_display = ('name_item', 'amount_item', 'request_hash_id')
+    readonly_fields = ('request_hash_id',)
+
+
+admin.site.register(Description, DescriptionAdmin)
