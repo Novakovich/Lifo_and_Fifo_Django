5bdf2a8dba955a5f52ee3d50c554d79bab542a81
Maksim <teramor1986@gmail.com>
2022-08-26 13:13:48 +0300

    Django CharityBar in Admin model added search_fields, list_filter

--- donation/admin.py
+++ donation/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
-from donation.models import Description
+from donation.models import Description, RequestItem, DonateItem
 
 
-class DescriptionAdmin(admin.ModelAdmin):
-    list_display = ('name_item', 'amount_item', 'request_hash_id', 'condition')
+class DonateItemAdmin(admin.ModelAdmin):
+    list_display = ('name_item', 'amount_item', 'request_hash_id', 'state',)
+    search_fields = ['request_hash_id__exact']
+    list_filter = ('state',)
     fieldsets = (
         ('Main info', {
             'fields': ('name_item',)
         }),
         ('Additional info', {
-            'fields': ('amount_item', 'request_hash_id', 'condition')
+            'fields': ('amount_item', 'request_hash_id', 'state',)
         }),
     )
     readonly_fields = ('request_hash_id',)
 
 
-admin.site.register(Description, DescriptionAdmin)
+admin.site.register(DonateItem, DonateItemAdmin)
