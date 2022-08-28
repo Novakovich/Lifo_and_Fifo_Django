dcb16809b914ce35c89f60b5d913c97b1c819f85
Maksim <teramor1986@gmail.com>
2022-08-28 16:39:25 +0300

    Django CharityBar in Admin model added action

--- donation/admin.py
+++ donation/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from donation.models import Description, RequestItem, DonateItem
+from donation.models import RequestItem, Description
 
 
-class DonateItemAdmin(admin.ModelAdmin):
+@admin.action(description='Mark selected items as used')
+def make_used(modeladmin, request, queryset):
+    queryset.update(condition='Used')
+
+
+class DescriptionAdmin(admin.ModelAdmin):
+    list_display = ('name_item', 'amount_item', 'request_hash_id', 'state', 'condition', )
+    search_fields = ['request_hash_id__exact']
+    list_filter = ('state',)
+    fieldsets = (
+        ('Main info', {
+            'fields': ('name_item',)
+        }),
+        ('Additional info', {
+            'fields': ('amount_item', 'request_hash_id', 'state', 'condition',)
+        }),
+    )
+    readonly_fields = ('request_hash_id',)
+    actions = [make_used]
+
+
+admin.site.register(Description, DescriptionAdmin)
+
+
+class RequestItemAdmin(admin.ModelAdmin):
     list_display = ('name_item', 'amount_item', 'request_hash_id', 'state',)
     search_fields = ['request_hash_id__exact']
     list_filter = ('state',)
@@ -17,4 +41,4 @@ class DonateItemAdmin(admin.ModelAdmin):
     readonly_fields = ('request_hash_id',)
 
 
-admin.site.register(DonateItem, DonateItemAdmin)
+admin.site.register(RequestItem, RequestItemAdmin)
